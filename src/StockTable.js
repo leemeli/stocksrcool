41da93573b18d83494e350056455f5b2b5c00010
Melody Lee <leemeli@uw.edu>
2016-11-30 11:00:25 -0800

    Fix React bugs

--- src/StockTable.js
+++ src/StockTable.js
@@ -3,13 +3,15 @@ import React from 'react';
 export default class StockTable extends React.Component {
     render(){
         return(
-            <table styleName="width:100%">
+            <table>
+            <tbody>
             <tr>
                 <th>Company</th>
                 <th>Close Price</th> 
                 <th>Net Change</th>
             </tr>
                 {/* Populate table with stock information from API here*/}
+                </tbody>
              </table>
         );
     }
