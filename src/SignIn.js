504cfffba8994437448dbe95792a3c60082c1b98
Melody Lee <leemeli@uw.edu>
2016-11-30 12:06:11 -0800

    Implement firebase sign up

--- src/SignIn.js
+++ src/SignIn.js
@@ -31,47 +31,6 @@ class SignIn extends React.Component {
                         </div>
                     </div>
                 </header>
-                <div className="container">
-                    <div className="row header">
-                        <h3>Start exploring our virtual stock market by signing up!</h3>
-                    </div>
-                    <div className="row body">
-                        <form action="#">
-                            <ul>
-                                <li>
-                                    <p className="left">
-                                        <label>Name</label>
-                                        <input type="text" name="first_name" placeholder="Jack Johnson" />
-                                    </p>
-                                </li>
-                                <li>
-                                    <p className="pull-right">
-                                        <label>Email <span className="req">*</span>
-                                        </label>
-                                        <input type="text" name="last_name" placeholder="jack.johnson@gmail.com" />
-                                    </p>
-                                </li>
-                                <li>
-                                    <p className="left">
-                                        <label>Desired Username</label>
-                                        <input type="text" name="username" placeholder="JackJohnson123" />
-                                    </p>
-                                </li>
-                                <li>
-                                    <p className="pull-right">
-                                        <label>Password</label>
-                                        <input type="password" name="password" placeholder="Password" />
-                                    </p>
-                                </li>
-                                <li>
-                                    <input className="btn btn-submit" type="submit" value="Register" />
-                                </li>
-                            </ul>
-                        </form>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore cumque, obcaecati ex eveniet reiciendis, quaerat placeat itaque totam magnam aperiam repellat quis possimus perspiciatis debitis incidunt. Natus molestiae non nobis.</p>
-                    <p>Excepturi, vitae veniam doloremque nihil reprehenderit adipisci, perspiciatis, assumenda laudantium molestiae blanditiis tempore saepe odio. Ipsum nobis, nulla molestias. Alias dolorem voluptatem, voluptatibus eveniet, aspernatur nesciunt cumque iste sint sed.</p>
-                </div>
             </div>
         );
     }
