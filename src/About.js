ef02747c055f0643914f21078d93f61fa589988a
greycabb <greycabbage@gmail.com>
2016-12-05 12:08:01 -0800

    more style for the front page

--- src/About.js
+++ src/About.js
@@ -4,10 +4,10 @@ class About extends React.Component {
     render() {
         return (
             <div className="container" id="aboutPage">
-                    <div className="row header">
-                        <h3>Start exploring our virtual stock market by signing up!</h3>
-                    </div>
-                    
+                <div className="row header">
+                    <h2>Start exploring our virtual stock market by signing up!</h2>
+                </div>
+                <div className="about">
                     <p>
                         In light of recent political events, now more than ever it becomes more apparent that you should vote with your wallet, and this is the most true in the stock market. Investing in stocks is investing in a company, and people need to be informed on who their money is going to.
                     </p>
@@ -18,8 +18,9 @@ class About extends React.Component {
                         With this app, you'll get experience with real-world stocks and get used to dealing with imaginary stocks, and see as your net worth increases or decreases based on price fluctuations that actually took place, all the while making sure that your money isn't funding companies whose ideals you do not support.
                     </p>
                     <p>
-                        We speculate that you will become very knowledgable about the world of stocks and the political affiliations of their companies!
+                        We <em>speculate</em> that you will become very knowledgable about the world of stocks and the political affiliations of their companies!
                     </p>
+                </div>
             </div>
         );
     }
