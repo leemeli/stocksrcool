98dea015db6e80a3c3856f5682016414df40d6c4
Melody Lee <leemeli@uw.edu>
2016-12-07 11:29:24 -0800

    Remove political tab

--- src/PoliticalBar.js
+++ src/PoliticalBar.js
@@ -34,11 +34,6 @@ export default class PoliticalBar extends React.Component {
     render() {
         return (
             <section role="region" id="politicalBar">
-                <ul className="nav nav-tabs">
-                    <li role="presentation" className={this.state.np}><div className="no-preference btn btn-default btn-lg" onClick={()=>{ this.props.oc3(); this.focusNP() }}>View All</div></li>
-                    <li role="presentation" className={this.state.d}><div className="democrat btn btn-default btn-lg" onClick={()=>{ this.props.oc1(); this.focusD() }}>Democrat</div></li>
-                    <li role="presentation" className={this.state.r}><div className="republican btn btn-default btn-lg" onClick={()=>{ this.props.oc2(); this.focusR() }}>Republican</div></li>
-                </ul>
                 <form className="navbar-form navbar-right" role="search">
                     <div className="form-group">
                         <input type="text" className="form-control" placeholder="Search" name="currentStock" onChange={this.props.change} />
