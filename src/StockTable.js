4c5a68883f7de57d07f8bd244c79db44edc8538b
greycabb <greycabbage@gmail.com>
2016-12-07 01:13:37 -0800

    added company code to table rows

--- src/StockTable.js
+++ src/StockTable.js
@@ -2,12 +2,19 @@ import React from 'react';
 import StockTableRow from './StockTableRow';
 
 export default class StockTable extends React.Component {
+
+    // Pick 5 randomly from our database
+    componentDidMount() {
+
+    }
+
     render() { 
         return(
             <section role="region" id="stock-table-region">
                 <table className="table table-bordered table-hover stock-table" aria-live="polite">
                     <tbody>
                     <tr id="firstRow">
+                        <th>Code</th>
                         <th>Company</th>
                         <th>Affiliation</th>
                         <th>Close Price</th> 
@@ -16,6 +23,10 @@ export default class StockTable extends React.Component {
                     </tr>
                         {/* Populate table with stock information from API here*/}
                         
+                        <StockTableRow name={this.props.name} stock={this.props.stock} stockCode={this.props.stockCode}/>
+                        <StockTableRow name={this.props.name} stock={this.props.stock} stockCode={this.props.stockCode}/>
+                        <StockTableRow name={this.props.name} stock={this.props.stock} stockCode={this.props.stockCode}/>
+                        <StockTableRow name={this.props.name} stock={this.props.stock} stockCode={this.props.stockCode}/>
                         <StockTableRow name={this.props.name} stock={this.props.stock} stockCode={this.props.stockCode}/>
 
                     </tbody>
--- src/StockTableRow.js
+++ src/StockTableRow.js
@@ -299,6 +299,7 @@ class StockTableRow extends React.Component {
 
         return (
             <tr>
+                <td>{this.props.stockCode}</td>
                 <td>{shortName}</td>
                 <td className={affiliationStyle}>{affiliation}</td>
                 <td>${closePrice}</td>
@@ -308,7 +309,7 @@ class StockTableRow extends React.Component {
                         (!this.state.buying && !this.state.selling) &&
                         <div>
                             <button className="btn btn-primary buy-sell" onClick={this.startBuy}>Buy</button>
-                            <button className="btn btn-default buy-sell" disabled={this.state.count < 1} onClick={this.startSell}>Sell ({this.state.count})</button>
+                            <button className="btn btn-default buy-sell" disabled={this.state.count < 1} onClick={this.startSell}>Sell</button>
                         </div>
                     }
 
